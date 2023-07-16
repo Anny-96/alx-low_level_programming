24033e57d26b855581353ec6a0306110de568981
Anny-96 <annymic816@gmail.com>
2023-07-16 09:51:40 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	int i;
+int i;
 for (i = 48; i < 58; i++)
 	{
 	putchar(i);
@@ -16,5 +16,5 @@ for (i = 48; i < 58; i++)
 	putchar(i);
 	}
 	putchar('\n');
-	return (0);
+return (0);
 }		
